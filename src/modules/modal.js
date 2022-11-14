437fcc44f7fa7f5f1531971febee54fb4aa06747
Mur <murmavilink@gmail.com>
2022-11-14 22:24:59 +0600

    сделал 1 и 2 пункт по тз (modal)

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,6 +1,31 @@
 
 const modal = () => {
-    console.log('ss');
+    const callbackBtns = document.querySelectorAll('.callback-btn');
+    const modalCallback = document.querySelector('.modal-callback');
+    const modalOverlay = document.querySelector('.modal-overlay');
+    const modalClose = modalCallback.querySelector('.modal-close');
+
+
+    callbackBtns.forEach(callbackBtn => {
+        callbackBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            modalCallback.style.display = 'block'
+            modalOverlay.style.display = 'block'
+        });
+    });
+
+
+    modalClose.addEventListener('click', () => {
+        modalCallback.style.display = 'none';
+        modalOverlay.style.display = 'none';
+    });
+
+    modalOverlay.addEventListener('click', (e) => {
+        modalOverlay.style.display = 'none';
+        modalCallback.style.display = 'none';
+    });
+
+
 }
 
 
