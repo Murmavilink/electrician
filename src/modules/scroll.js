0d6aac25a4e7b04bdc4697de9b44b06b5ad55e37
Mur <murmavilink@gmail.com>
2022-11-15 15:01:01 +0600

    написал плавный scroll

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,5 +1,25 @@
 const scroll = () => {
+    const topMenu = document.querySelector('.top-menu');
+
+    topMenu.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        if(e.target.matches('li a')) {
+            let link = e.target.closest('a');
+            console.log(link);
+
+            const id = link.getAttribute('href').substring(1);
+            const section = document.getElementById(id)
     
+            if(section) {
+                section.scrollIntoView({
+                    block: "center", 
+                    behavior: "smooth"
+                });
+            }
+      
+        } 
+    })
 }
 
 export default scroll
