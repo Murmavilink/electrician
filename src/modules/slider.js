9dce341532d1a90c6977cfeb4cf5ee5dafe19490
Mur <murmavilink@gmail.com>
2022-11-16 11:55:56 +0600

    сделал слайдер swiper

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,7 +1,32 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 const slider = () => {
 
-    console.log('slider');
-
+    const swiper = new Swiper('.top-slider-swiper', {
+        modules: [Navigation, Pagination],
+        loop: true,
+        // speed: 2000,
+        // autoplay: {
+        //   enabled: true,
+        //   delay: 1,
+        // },
+        // spaceBetween: 30,
+        // centeredSlides: true,
+        // loop: true,
+        autoplay: {
+          delay: 3000,
+        //   disableOnInteraction: false,
+        },
+        // slidesPerView: 1,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+      });
 }
 
 
