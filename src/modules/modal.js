cdfa0b8ebc5c158df072a1c05426cc5b0b7681fd
Mur <murmavilink@gmail.com>
2022-11-17 13:34:21 +0600

    сделал 5 пункт тз

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,13 +1,15 @@
 
 const modal = () => {
     const callbackBtns = document.querySelectorAll('.callback-btn');
+    const carouselElements = document.querySelectorAll('.services-carousel a');
     const modalCallback = document.querySelector('.modal-callback');
     const modalOverlay = document.querySelector('.modal-overlay');
     const modalClose = modalCallback.querySelector('.modal-close');
 
+    const arrayElements = [...callbackBtns, ...carouselElements];
 
-    callbackBtns.forEach(callbackBtn => {
-        callbackBtn.addEventListener('click', (e) => {
+    arrayElements.forEach(element => {
+        element.addEventListener('click', (e) => {
             e.preventDefault()
             modalCallback.style.display = 'block'
             modalOverlay.style.display = 'block'
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,25 +1,43 @@
 import { animate } from './helpers'
-import Swiper, {Pagination, Autoplay } from 'swiper';
+import Swiper, {Navigation, Pagination, Autoplay } from 'swiper';
 
 const slider = () => {
 
-  const tables = document.querySelectorAll('.table')
+	const tables = document.querySelectorAll('.table')
 
-    const swiper = new Swiper('.top-slider-swiper', {
-        modules: [Pagination, Autoplay],
-        loop: true,
-        autoplay: {
-          delay: 3000,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        }
-      });
+	const swiper = new Swiper('.top-slider-swiper', {
+		modules: [Pagination, Autoplay],
+		loop: true,
+		autoplay: {
+			delay: 3000,
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		}
+	});
 
-      tables.forEach(table => {  
-        table.classList.add('active')
-      })
+	tables.forEach(table => {
+		table.classList.add('active')
+	})
+
+
+	///////////////////////////////////////////////////////////////////////////
+
+
+	const swiperCarusel = new Swiper('.services-elements', {
+		modules: [Navigation, Pagination, Autoplay],
+		loop: true,
+		slidesPerView: 3,
+		autoplay: {
+			delay: 3000,
+		},
+		navigation: {
+			nextEl: ".arrow-right",
+			prevEl: ".arrow-left",
+		  },
+	
+	});
 
 
 }
