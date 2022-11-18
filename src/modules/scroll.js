b0d176bf50572d252e199c9e7beb2281f67921a3
Mur <murmavilink@gmail.com>
2022-11-18 13:04:22 +0600

    сделал 8 пункт scroll

--- src/modules/scroll.js
+++ src/modules/scroll.js
@@ -1,24 +1,43 @@
 const scroll = () => {
     const topMenu = document.querySelector('.top-menu');
+    const upBtn = document.querySelector('.up');
+    const upScroll = document.getElementById('upScroll');
+
+
+    const scrollToSection = (section) => {
+        if(section) {
+            section.scrollIntoView({
+                block: "center", 
+                behavior: "smooth"
+            });
+        }
+    }
 
     topMenu.addEventListener('click', (e) => {
         e.preventDefault();
-
         if(e.target.matches('li a')) {
             let link = e.target.closest('a');
-
             const id = link.getAttribute('href').substring(1);
             const section = document.getElementById(id);
-    
-            if(section) {
-                section.scrollIntoView({
-                    block: "center", 
-                    behavior: "smooth"
-                });
-            }
-      
+            
+            scrollToSection(section);
         } 
     });
+
+
+    window.addEventListener('scroll', () => {
+        if(document.documentElement.scrollTop > 500) {
+            upBtn.style.display = 'block';
+        } else {
+            upBtn.style.display = 'none';
+        }
+    });
+
+
+    upBtn.addEventListener('click', () => {
+        scrollToSection(upScroll)
+    });
+    
 }
 
 export default scroll
